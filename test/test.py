2abcd343799829a7f75901fdf12725a05af0cfef
myint <macbookpro.local>
2012-11-24 16:08:01 -0800

    Correct paths

--- test/test.py
+++ test/test.py
@@ -10,8 +10,9 @@ ROOT_DIR = os.path.split(os.path.abspath(os.path.dirname(__file__)))[0]
 
 
 def main():
-    subprocess.check_call(['./heatmap.py', '-b', 'black',
-                           '-p', 'test/graffiti.coords',
+    subprocess.check_call([os.path.join(ROOT_DIR, 'heatmap.py'),
+                           '-b', 'black',
+                           '-p', os.path.join(ROOT_DIR, 'test', 'graffiti.coords'),
                            '-r', '30', '-W', '300',
                            '-o', os.path.join(ROOT_DIR, 'test', 'g1.ppm'),
                            '-P', 'equirectangular'])
