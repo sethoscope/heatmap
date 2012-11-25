24a9e64212341b4330c483ef09aa653cc6940ebc
myint <macbookpro.local>
2012-11-24 16:03:55 -0800

    Move test to a separate file

---
+++ test/test.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+"""Test case."""
+
+import os
+import subprocess
+import sys
+
+
+ROOT_DIR = os.path.split(os.path.abspath(os.path.dirname(__file__)))[0]
+
+
+def main():
+    subprocess.check_call(['./heatmap.py', '-b', 'black',
+                           '-p', 'test/graffiti.coords',
+                           '-r', '30', '-W', '300',
+                           '-o', os.path.join(ROOT_DIR, 'test', 'g1.ppm'),
+                           '-P', 'equirectangular'])
+
+    subprocess.check_call(['perceptualdiff',
+                           os.path.join(ROOT_DIR, 'test', 'graffiti.ppm'),
+                           os.path.join(ROOT_DIR, 'test', 'g1.ppm')])
+
+
+if __name__ == '__main__':
+    sys.exit(main())
