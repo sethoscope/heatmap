4a0ae97b691ed4b0ee4b5621757c8fdf8c7a1631
myint <macbookpro.local>
2012-11-30 11:29:41 -0800

    Use unittest to show progress message

--- test/test.py
+++ test/test.py
@@ -3,25 +3,35 @@
 
 import os
 import subprocess
-import sys
+
+try:
+    # Python 2.6
+    import unittest2 as unittest
+except:
+    import unittest
 
 
 ROOT_DIR = os.path.split(os.path.abspath(os.path.dirname(__file__)))[0]
 
-def main():
 
-    subprocess.check_call([os.path.join(ROOT_DIR, 'heatmap.py'),
-                           '-p', os.path.join(ROOT_DIR, 'test', 'few-points'),
-                           '-b', 'black',
-                           '-r', '3',
-                           '-W', '22',
-                           '-P', 'equirectangular',
-                           '-o', os.path.join(ROOT_DIR, 'test', 'output.ppm')])
+class Tests(unittest.TestCase):
 
-    subprocess.check_call(['perceptualdiff',
-                           os.path.join(ROOT_DIR, 'test', 'few-points.ppm'),
-                           os.path.join(ROOT_DIR, 'test', 'output.ppm')])
+    def test_system(self):
+
+        subprocess.check_call(
+            [os.path.join(ROOT_DIR, 'heatmap.py'),
+             '-p', os.path.join(ROOT_DIR, 'test', 'few-points'),
+             '-b', 'black',
+             '-r', '3',
+             '-W', '22',
+             '-P', 'equirectangular',
+             '-o', os.path.join(ROOT_DIR, 'test', 'output.ppm')])
+
+        subprocess.check_call(
+            ['perceptualdiff',
+             os.path.join(ROOT_DIR, 'test', 'few-points.ppm'),
+             os.path.join(ROOT_DIR, 'test', 'output.ppm')])
 
 
 if __name__ == '__main__':
-    sys.exit(main())
+    unittest.main()
