684b1ddcc4a2f2b9f20a7c7f1538ab50b3f9fd2c
Seth Golub <seth@sethoscope.net>
2014-02-18 13:50:43 -0800

    add test for GPX file processing

--- Makefile
+++ Makefile
@@ -1,13 +1,15 @@
 all: tests python3_tests
 
 tests:
-	test/test_animation.py
 	test/test.py
+	test/test_gpx.py
 	test/test_gradients.py
 	test/test_projection_scale.py
+	test/test_animation.py
 
 python3_tests:
-	python3 test/test_animation.py
 	python3 test/test.py
+	python3 test/test_gpx.py
 	python3 test/test_gradients.py
 	python3	test/test_projection_scale.py
+	python3 test/test_animation.py
---
+++ test/test_gpx.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+"""Test case."""
+
+import os
+import subprocess
+import unittest
+import sys
+
+ROOT_DIR = os.path.split(os.path.abspath(os.path.dirname(__file__)))[0]
+sys.path.append(ROOT_DIR)
+import heatmap as hm
+
+
+class Tests(unittest.TestCase):
+
+    def test_system(self):
+        output_file = os.path.join(ROOT_DIR, 'test', 'output.ppm')
+        try:
+            subprocess.check_call(
+                [os.path.join(ROOT_DIR, 'heatmap.py'),
+                 '-g', os.path.join(ROOT_DIR, 'test', 'smile.gpx'),
+                 '-b', 'black',
+                 '-r', '3',
+                 '-W', '22',
+                 '-P', 'equirectangular',
+                 '-o', output_file])
+
+            subprocess.check_call(
+                ['perceptualdiff',
+                 os.path.join(ROOT_DIR, 'test', 'smile-gpx.ppm'),
+                 output_file])
+        finally:
+            try:
+                os.remove(output_file)
+            except OSError:
+                pass  # perhaps it was never created
+
+
+if __name__ == '__main__':
+    unittest.main()
