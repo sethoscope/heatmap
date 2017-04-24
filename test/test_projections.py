8cb65a8a2ffbf464c3414be5187523f5ecf9169e
hugovk <hugovk@users.noreply.github.com>
2017-04-24 22:57:25 +0300

    Drop support for Python 2.6

--- test/test_projections.py
+++ test/test_projections.py
@@ -3,11 +3,7 @@
 
 import os
 import sys
-
-try:
-    import unittest2 as unittest  # Python 2.6
-except ImportError:
-    import unittest
+import unittest
 
 ROOT_DIR = os.path.split(os.path.abspath(os.path.dirname(__file__)))[0]
 sys.path.append(ROOT_DIR)
