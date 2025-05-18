c0b2aacd558cd2f8f6cd1a12ad5d1da51f5a6868
Seth Golub <entropy@gmail.com>
2025-05-17 19:07:10 -0700

    Add system tests using background image
    Added two tests, one with a background image, and one with the same
    image darkened using -B.

---
+++ test/test_background.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+"""Test case."""
+
+import os
+import subprocess
+import sys
+import unittest
+from helper import TestHeatmap, TEST_DIR
+
+
+class Tests(TestHeatmap):
+
+    def test_system_background(self):
+        '''Test system with a background image.'''
+        output_file = os.path.join(TEST_DIR, 'output.png')
+        try:
+            self.helper_run(
+                ['-I', os.path.join(TEST_DIR, 'playa.jpg'),
+                 '-r', '10',
+                 '-o', output_file,
+                 os.path.join(TEST_DIR, 'few-points')])
+
+            subprocess.check_call(
+                ['perceptualdiff',
+                 os.path.join(TEST_DIR, 'with-background.png'),
+                 output_file])
+        finally:
+            try:
+                os.remove(output_file)
+            except OSError:
+                pass  # perhaps it was never created
+
+    def test_system_background_dark(self):
+        '''Test system with a background image.'''
+        output_file = os.path.join(TEST_DIR, 'output.png')
+        try:
+            self.helper_run(
+                ['-I', os.path.join(TEST_DIR, 'playa.jpg'),
+                 '-r', '10',
+                 '-B', '0.4',
+                 '-o', output_file,
+                 os.path.join(TEST_DIR, 'few-points')])
+
+            subprocess.check_call(
+                ['perceptualdiff',
+                 os.path.join(TEST_DIR, 'with-background-dark.png'),
+                 output_file])
+        finally:
+            try:
+                os.remove(output_file)
+            except OSError:
+                pass  # perhaps it was never created
+
+
+if __name__ == '__main__':
+    unittest.main()
